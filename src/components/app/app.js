dbea802531e0d6c787c0efa793f017006f862535
Vitaliy Demyanenko <padre.vitaliy@gmail.com>
2022-09-16 19:01:52 +0300

    added all functionality

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -10,37 +10,37 @@ import EmployersAddForm from '../employers-add-form/employers-add-form';
 import './app.css';
 
 class App extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             data: [
-                {name: 'John', salary: 3000, increase: false, id: uuidv4()},
-                {name: 'Veronika', salary: 2000, increase: false, id: uuidv4()},
-                {name: 'Max', salary: 1500, increase: false, id: uuidv4()}
+                { name: 'John', salary: 3000, increase: false, rise: true, id: uuidv4() },
+                { name: 'Veronika', salary: 2000, increase: true, rise: false, id: uuidv4() },
+                { name: 'Max', salary: 1500, increase: false, rise: false, id: uuidv4() }
             ]
         }
 
     }
-    
 
     deleteItem = (id) => {
-        this.setState(({data}) => {
+        this.setState(({ data }) => {
             return {
                 data: data.filter(item => item.id !== id)
             }
         })
-        
+
         console.log(id);
     }
 
     addItem = (name, salary) => {
         const newItem = {
-            name, 
+            name,
             salary,
             increase: false,
+            rise: false,
             id: uuidv4()
         }
-        this.setState(({data}) => {
+        this.setState(({ data }) => {
             const newArr = [...data, newItem];
             return {
                 data: newArr
@@ -48,25 +48,35 @@ class App extends Component {
         });
     }
 
-    render(){
+    onToggleProp = (id, prop) => {
+       this.setState(({ data }) => ({
+            data: data.map(item => item.id === id ? {...item, [prop]: !item[prop]} : item)
+        }))
+    }
 
-        return(
+    render() {
+        const emploeese = this.state.data.length;
+        const increased = this.state.data.filter(item => item.increase).length;
+        return (
             <div className="app">
-                <AppInfo/>
-    
+                <AppInfo
+                emploeese={emploeese}
+                increased={increased}/>
+
                 <div className="search-panel">
-                    <SearchPanel/>
-                    <AppFilter/>
+                    <SearchPanel />
+                    <AppFilter />
                 </div>
-    
-                <EmployersList 
-                data={this.state.data}
-                onDelete={this.deleteItem}/>
-                <EmployersAddForm 
-                onAdd ={this.addItem}/>
+
+                <EmployersList
+                    data={this.state.data}
+                    onDelete={this.deleteItem}
+                    onToggleProp={this.onToggleProp} />
+                <EmployersAddForm
+                    onAdd={this.addItem} />
             </div>
         );
-    }    
+    }
 }
 
 
